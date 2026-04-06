ca00c5735e6601c7fb0d3620561a1987ff92e239
Kanthasri <140141121+Kanthasri@users.noreply.github.com>
2026-04-06 23:37:20 +0530

    Update Solution.java

--- LeetCode/Target-Sum/Solution.java
+++ LeetCode/Target-Sum/Solution.java
@@ -1,14 +1,17 @@
-├── (+a)
-│   ├── (+b)^
-│   │   ├── (+c)
-│   │   └── (-c)
-│   └── (-b)~
-│       ├── (+c)
-│       └── (-c)
-└── (-a)
-    ├── (+b)^
-    │   ├── (+c)
-    │   └── (-c)
-    └── (-b)~
-        ├── (+c)
-        └── (-c)
+class Solution {
+    int ways = 0;
+    public int findTargetSumWays(int[] nums, int target) {
+        dfs(nums,0,0,target);
+        return ways;
+    }
+    void dfs(int []arr,int idx,int sum,int tar){
+        if(idx==arr.length){
+            if(sum==tar){
+                ways++;
+            }
+            return;
+        }
+        dfs(arr,idx+1,sum+arr[idx],tar);
+        dfs(arr,idx+1,sum-arr[idx],tar);
+    }
+}
