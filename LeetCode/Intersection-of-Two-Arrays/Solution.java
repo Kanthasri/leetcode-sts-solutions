cd460cc5461ad996468fc8c8984671dfbc492d71
Kanthasri <140141121+Kanthasri@users.noreply.github.com>
2026-04-06 23:23:19 +0530

    Update Solution.java

--- LeetCode/Intersection-of-Two-Arrays/Solution.java
+++ LeetCode/Intersection-of-Two-Arrays/Solution.java
@@ -1,9 +1,23 @@
-class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        set1 = set(nums1)
-        ans = []
-        for i in nums2:
-            if i in set1:
-                ans.append(i)
-                set1.remove(i)
-        return ans
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+      Set<Integer> set=new HashSet<>();
+      Set<Integer> res=new HashSet<>();
+    for(int i=0;i<nums1.length;i++){
+        set.add(nums1[i]);
+    }
+
+    for(int i=0;i<nums2.length;i++){
+        if(set.contains(nums2[i])){
+            res.add(nums2[i]);
+        }
+
+    }
+    List<Integer> list=new ArrayList<>(res);
+    int [] arr=new int[list.size()];
+    for(int i=0;i<list.size();i++){
+            arr[i]=list.get(i);
+    }
+    return arr;
+
+}
+}
