dffb2fc3b7002656933b7d71cb023569b051bd70
Kanthasri <140141121+Kanthasri@users.noreply.github.com>
2026-07-22 18:36:36 +0530

    Add solution for Subarray Sum Equals K

---
+++ LeetCode/Subarray-Sum-Equals-K/Solution.cpp
@@ -0,0 +1,4 @@
+Let's take an example not given in the question by taking negative numbers
+Suppose our arr is arr[]: [-1, -1, 1]  && k = 0
+
+So, the answer should be '1' as their is only one subarray whose sum is 0 i.e (-1 + 1)
